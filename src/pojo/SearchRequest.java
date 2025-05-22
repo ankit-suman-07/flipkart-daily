e5d01d039d276c59a73f53723dbf877b008db2fc
Ankit Suman <105882103+ankit-suman-07@users.noreply.github.com>
2025-05-22 11:40:12 +0530

    Add POJO classes

--- src/pojo/SearchRequest.java
+++ src/pojo/SearchRequest.java
@@ -1,4 +1,30 @@
 package src.pojo;
 
+import java.util.List;
+
 public class SearchRequest {
+    private List<String> brand;
+    private List<String> category;
+    private Integer priceFrom;
+    private Integer priceTo;
+    private String orderBy;
+    private boolean asc;
+
+    public List<String> getBrand() { return brand; }
+    public void setBrand(List<String> brand) { this.brand = brand; }
+
+    public List<String> getCategory() { return category; }
+    public void setCategory(List<String> category) { this.category = category; }
+
+    public Integer getPriceFrom() { return priceFrom; }
+    public void setPriceFrom(Integer priceFrom) { this.priceFrom = priceFrom; }
+
+    public Integer getPriceTo() { return priceTo; }
+    public void setPriceTo(Integer priceTo) { this.priceTo = priceTo; }
+
+    public String getOrderBy() { return orderBy; }
+    public void setOrderBy(String orderBy) { this.orderBy = orderBy; }
+
+    public boolean isAsc() { return asc; }
+    public void setAsc(boolean asc) { this.asc = asc; }
 }
--- src/pojo/SearchResponse.java
+++ src/pojo/SearchResponse.java
@@ -1,4 +1,16 @@
 package src.pojo;
 
+import src.model.Item;
+
+import java.util.List;
+
 public class SearchResponse {
+    private List<Item> items;
+
+    public SearchResponse(List<Item> items) {
+        this.items = items;
+    }
+
+    public List<Item> getItems() { return items; }
+    public void setItems(List<Item> items) { this.items = items; }
 }
